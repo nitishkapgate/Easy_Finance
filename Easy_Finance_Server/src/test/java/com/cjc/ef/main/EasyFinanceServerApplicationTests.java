4ce8e84522329600d3c9b11c2e64ee320ceab6ef
nitishkapgate <145214494+nitishkapgate@users.noreply.github.com>
2023-09-28 17:27:50 +0530

    Easy_Finance_Server Added

---
+++ Easy_Finance_Server/src/test/java/com/cjc/ef/main/EasyFinanceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjc.ef.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFinanceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
